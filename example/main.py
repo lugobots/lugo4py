d8b6dbe8aaa6908214bcd3f2b05c388eabca5119
rubens21 <rubens21@gmail.com>
2023-01-29 16:52:36 -0700

    fixing docker file reference, fixing concurrence issue on starting up

--- example/main.py
+++ example/main.py
@@ -1,16 +1,24 @@
 import os, threading
+import random
 from src.main import LugoClient
 from src.protos import physics_pb2
 
-def test(*args):
-   print(args)
+
+def test(*args: server_pb2.GameSnapshot):
+    print(args.turn)
+    #     print("cool")
 
 
 def one():
-    client = LugoClient.new_client(physics_pb2.Point(x=2200, y=5000))
+    x = random.randint(0, 100) + 2000
+    y = random.randint(0, 100) + 5000
+    if int(os.environ.get("BOT_NUMBER")) == 1:
+        x = 0
+
+    print("here we go")
+    client = LugoClient.new_client(physics_pb2.Point(x=x, y=y))
     client.play(test)
 
 
 if __name__ == "__main__":
     one()
-
