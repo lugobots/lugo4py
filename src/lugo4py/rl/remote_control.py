cf996bbb6fa426703c008396313bb270bb549a54
Ddarckwolf <aquersouza@gmail.com>
2023-03-29 14:54:58 -0300

    remake resumeListening function and fix identation

--- src/lugo4py/rl/remote_control.py
+++ src/lugo4py/rl/remote_control.py
@@ -4,6 +4,7 @@ from ..protos import remote_pb2_grpc
 from ..protos.remote_pb2 import PauseResumeRequest, BallProperties, NextTurnRequest, PlayerProperties, GameProperties, ResumeListeningRequest
 from ..protos.physics_pb2 import Point, Velocity
 from ..protos.physics_pb2 import GameSnapshot, Team
+import asyncio
 
 
 class RemoteControl(object):
@@ -11,38 +12,29 @@ class RemoteControl(object):
     def __init__(self):
         self.client = None
 
+    async def connect(self, grpcAddress: str) -> None:
+        try:
+            self.client = remote.RemoteClient(
+                grpcAddress, grpc.credentials.create_insecure())
+            deadline = datetime.now() + timedelta(seconds=5)
+            await self.client.wait_for_ready(deadline=deadline)
+        except grpc.RpcError as e:
+            print("ERROR: ", e)
+            raise e
 
-async def connect(self, grpcAddress: str) -> None:
-    try:
-        self.client = remote.RemoteClient(
-            grpcAddress, grpc.credentials.create_insecure())
-        deadline = datetime.now() + timedelta(seconds=5)
-        await self.client.wait_for_ready(deadline=deadline)
-    except grpc.RpcError as e:
-        print("ERROR: ", e)
-        raise e
-
-
-async def pauseResume(self):
-    pause_req = remote.PauseResumeRequest()
-    try:
-        self.client.pause_or_resume(pause_req)
-    except grpc.RpcError as e:
-        print(f"ERROR: {e}")
-        raise
+    async def pauseResume(self):
+        pause_req = remote.PauseResumeRequest()
+        try:
+            self.client.pause_or_resume(pause_req)
+        except grpc.RpcError as e:
+            print(f"ERROR: {e}")
+            raise
 
     async def resumeListening(self):
-        req = ResumeListeningRequest()
-        # return new Promise<void>((resolve, reject) => {
-        #     const resp = this.client.resumeListeningPhase(req, (err) => {
-        #         if (err) {
-        #             console.log(`ERROR: `, err)
-        #             reject(err)
-        #             return
-        #         }
-        #         resolve()
-        #     })
-        # })
+        request = remote.ResumeListeningRequest()
+        return await asyncio.create_task(
+            self.client.resumeListeningPhase(request)
+        )
 
     async def nextTurn(self):
         nextTurnReq = NextTurnRequest()
