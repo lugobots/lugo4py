79cc671bdc7107118e5c5278f4f48d4b26a10d3d
Ddarckwolf <aquersouza@gmail.com>
2023-03-31 11:30:24 -0300

    remake new helper player functions

--- src/lugo4py/rl/helper_bots.py
+++ src/lugo4py/rl/helper_bots.py
@@ -1,7 +1,8 @@
 import asyncio
 from ..mapper import Mapper
-from ..client import NewClientFromConfig
+from ..client import NewClientFromConfig, RawTurnProcessor
 from ..protos.server_pb2 import GameSnapshot, OrderSet
+from lugo4py.snapshot import GameSnapshotReader
 
 PLAYER_POSITIONS = {
     1: {'Col': 0, 'Row': 1},
@@ -36,3 +37,41 @@ async def newZombiePlayer(teamSide, playerNumber, gameServerAddress):
     except Exception as e:
         promise.set_exception(e)
     return promise
+
+
+async def newChaserHelperPlayer(teamSide, playerNumber, gameServerAddress):
+    async def turnHandler(orderSet: OrderSet, snapshot: GameSnapshot) -> OrderSet:
+        reader = GameSnapshotReader(snapshot, teamSide)
+        orderSet.addOrders(reader.makeOrderCatch())
+        me = reader.getPlayer(teamSide, playerNumber)
+        if not me:
+            raise ValueError("did not find myself in the game")
+
+        orderSet.addOrders(reader.makeOrderMoveMaxSpeed(
+            me.getPosition(), snapshot.getBall().getPosition()))
+        orderSet.setDebugMessage(
+            f"{ 'HOME' if teamSide == 0 else 'AWAY' }-{playerNumber} #{snapshot.getTurn()} - chasing ball")
+        return orderSet
+
+    return newCustomHelperPlayer(teamSide, playerNumber, gameServerAddress, turnHandler)
+
+
+async def newZombieHelperPlayer(teamSide, playerNumber, gameServerAddress):
+    async def turnHandler(orderSet: OrderSet, snapshot: GameSnapshot) -> OrderSet:
+        orderSet.setDebugMessage(
+            f"{ 'HOME' if teamSide == 0 else 'AWAY' }-{playerNumber} #{snapshot.getTurn()}")
+        return orderSet
+
+    return await newCustomHelperPlayer(teamSide, playerNumber, gameServerAddress, turnHandler)
+
+
+async def newCustomHelperPlayer(teamSide, playerNumber, gameServerAddress, turnHandler: RawTurnProcessor):
+    try:
+        map = Mapper(22, 5, teamSide)
+        initialRegion = map.getRegion(
+            PLAYER_POSITIONS[playerNumber]['Col'], PLAYER_POSITIONS[playerNumber]['Row'])
+        lugoClient = NewClientFromConfig(
+            gameServerAddress, True, "", teamSide, playerNumber, initialRegion.getCenter())
+        await lugoClient.play(turnHandler)
+    except Exception as e:
+        raise e
