1b0537a3c706d36adcdd8ea25f80adae5a5b5318
Ddarckwolf <aquersouza@gmail.com>
2023-04-08 20:30:10 -0300

    updating gym class

--- src/lugo4py/rl/gym.py
+++ src/lugo4py/rl/gym.py
@@ -9,10 +9,9 @@ import asyncio
 
 class Gym(object):
 
-    def __init__(self, lugo_client: LugoClient, trainer: BotTrainer, trainingFunction: TrainingFunction, debugging_log=False):
+    def __init__(self, remote_control: RemoteControl, trainer: BotTrainer, trainingFunction: TrainingFunction, debugging_log=False):
         self.gameServerAddress = ''
-        self.lugo_client = lugo_client
-        self.remoteControl = RemoteControl(lugo_client)
+        self.remoteControl = remote_control
         self.trainingController = TrainingCrl(
             self.remoteControl, BotTrainer, trainingFunction)
 
