65074558f7f4afdb16f2772e30d2b88411fcf8e9
Angelo Katipunan <lugobots@gmail.com>
2023-09-06 20:25:03 -0600

    updating read me

--- example/simple/my_bot.py
+++ example/simple/my_bot.py
@@ -4,64 +4,6 @@ from abc import ABC
 import src.lugo4py as lugo4py
 import src.lugo4py.mapper as mapper
 
-
-def get_my_expected_position(reader: lugo4py.GameSnapshotReader, my_mapper: mapper.Mapper, number: int):
-    mapper_cols = 10
-
-    player_tactic_positions = {
-        'DEFENSIVE': {
-            2: {'Col': 1, 'Row': 1},
-            3: {'Col': 2, 'Row': 2},
-            4: {'Col': 2, 'Row': 3},
-            5: {'Col': 1, 'Row': 4},
-            6: {'Col': 3, 'Row': 1},
-            7: {'Col': 3, 'Row': 2},
-            8: {'Col': 3, 'Row': 3},
-            9: {'Col': 3, 'Row': 4},
-            10: {'Col': 4, 'Row': 3},
-            11: {'Col': 4, 'Row': 2},
-        },
-        'NORMAL': {
-            2: {'Col': 2, 'Row': 1},
-            3: {'Col': 4, 'Row': 2},
-            4: {'Col': 4, 'Row': 3},
-            5: {'Col': 2, 'Row': 4},
-            6: {'Col': 6, 'Row': 1},
-            7: {'Col': 8, 'Row': 2},
-            8: {'Col': 8, 'Row': 3},
-            9: {'Col': 6, 'Row': 4},
-            10: {'Col': 7, 'Row': 4},
-            11: {'Col': 7, 'Row': 1},
-        },
-        'OFFENSIVE': {
-            2: {'Col': 3, 'Row': 1},
-            3: {'Col': 5, 'Row': 2},
-            4: {'Col': 5, 'Row': 3},
-            5: {'Col': 3, 'Row': 4},
-            6: {'Col': 7, 'Row': 1},
-            7: {'Col': 8, 'Row': 2},
-            8: {'Col': 8, 'Row': 3},
-            9: {'Col': 7, 'Row': 4},
-            10: {'Col': 9, 'Row': 4},
-            11: {'Col': 9, 'Row': 1},
-        }
-    }
-
-    ball_region = my_mapper.get_region_from_point(reader.get_ball().position)
-    field_third = mapper_cols / 3
-    ball_cols = ball_region.get_col()
-
-    team_state = "OFFENSIVE"
-    if ball_cols < field_third:
-        team_state = "DEFENSIVE"
-    elif ball_cols < field_third * 2:
-        team_state = "NORMAL"
-
-    expected_region = my_mapper.get_region(player_tactic_positions[team_state][number]['Col'],
-                                           player_tactic_positions[team_state][number]['Row'])
-    return expected_region.get_center()
-
-
 class MyBot(lugo4py.Bot, ABC):
 
     def __init__(self, side: lugo4py.TeamSide, number: int, init_position: lugo4py.Point, my_mapper: mapper.Mapper):
@@ -207,6 +149,61 @@ class MyBot(lugo4py.Bot, ABC):
     def getting_ready(self, snapshot: lugo4py.GameSnapshot):
         print('getting ready')
 
+def get_my_expected_position(reader: lugo4py.GameSnapshotReader, my_mapper: mapper.Mapper, number: int):
+    mapper_cols = 10
+
+    player_tactic_positions = {
+        'DEFENSIVE': {
+            2: {'Col': 1, 'Row': 1},
+            3: {'Col': 2, 'Row': 2},
+            4: {'Col': 2, 'Row': 3},
+            5: {'Col': 1, 'Row': 4},
+            6: {'Col': 3, 'Row': 1},
+            7: {'Col': 3, 'Row': 2},
+            8: {'Col': 3, 'Row': 3},
+            9: {'Col': 3, 'Row': 4},
+            10: {'Col': 4, 'Row': 3},
+            11: {'Col': 4, 'Row': 2},
+        },
+        'NORMAL': {
+            2: {'Col': 2, 'Row': 1},
+            3: {'Col': 4, 'Row': 2},
+            4: {'Col': 4, 'Row': 3},
+            5: {'Col': 2, 'Row': 4},
+            6: {'Col': 6, 'Row': 1},
+            7: {'Col': 8, 'Row': 2},
+            8: {'Col': 8, 'Row': 3},
+            9: {'Col': 6, 'Row': 4},
+            10: {'Col': 7, 'Row': 4},
+            11: {'Col': 7, 'Row': 1},
+        },
+        'OFFENSIVE': {
+            2: {'Col': 3, 'Row': 1},
+            3: {'Col': 5, 'Row': 2},
+            4: {'Col': 5, 'Row': 3},
+            5: {'Col': 3, 'Row': 4},
+            6: {'Col': 7, 'Row': 1},
+            7: {'Col': 8, 'Row': 2},
+            8: {'Col': 8, 'Row': 3},
+            9: {'Col': 7, 'Row': 4},
+            10: {'Col': 9, 'Row': 4},
+            11: {'Col': 9, 'Row': 1},
+        }
+    }
+
+    ball_region = my_mapper.get_region_from_point(reader.get_ball().position)
+    field_third = mapper_cols / 3
+    ball_cols = ball_region.get_col()
+
+    team_state = "OFFENSIVE"
+    if ball_cols < field_third:
+        team_state = "DEFENSIVE"
+    elif ball_cols < field_third * 2:
+        team_state = "NORMAL"
+
+    expected_region = my_mapper.get_region(player_tactic_positions[team_state][number]['Col'],
+                                           player_tactic_positions[team_state][number]['Row'])
+    return expected_region.get_center()
 
 PLAYER_POSITIONS = {
     1: {'Col': 0, 'Row': 0},
