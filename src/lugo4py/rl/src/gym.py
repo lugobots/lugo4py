6f242c4d4851dbba9e7e6075e6a542ab6fe14fb9
Rubens Silva <rubens21@gmail.com>
2025-03-29 12:34:22 -0600

    WIP - adapting the RL code to use RL assistant grpc service

--- src/lugo4py/rl/src/gym.py
+++ src/lugo4py/rl/src/gym.py
@@ -1,98 +1,50 @@
+import contextlib
+import grpc
 import time
+from typing import Any
 
-from .training_controller import TrainingCrl
-from .helper_bots import newChaserHelperPlayer, newZombieHelperPlayer
-from .remote_control import RemoteControl
-from .interfaces import BotTrainer, TrainingFunction
-from ...src import client
-from ...protos.server_pb2 import Team
-import threading
-
-from concurrent.futures import ThreadPoolExecutor
+from src.lugo4py.protos.remote_pb2_grpc import Remote
+from src.lugo4py.protos.rl_assistant_pb2 import RLSessionConfig
+from src.lugo4py.protos.rl_assistant_pb2_grpc import RLAssistant
+from src.lugo4py.rl import TrainingCrl
 
 
 class Gym:
+    def __init__(self, grpc_conn: grpc.Channel):
+        self.grpc_conn = grpc_conn
+        self.logger = lambda msg: print(f'set debugger')
+        self.assistant = RLAssistant(grpc_conn)
+        self.remote = Remote(grpc_conn)
 
-    def __init__(
-            self,
-            executor: ThreadPoolExecutor,
-            remote_control: RemoteControl,
-            trainer: BotTrainer,
-            trainingFunction: TrainingFunction,
-            options=None,
-    ):
-        if options is None:
-            options = {"debugging_log": False}
+    @classmethod
+    def new_gym(cls, config: "Config", logger: Logger) -> tuple["Gym", Remote]:
+        options = [grpc.insecure_channel(config.grpc_address)]
 
-        self.remoteControl = remote_control
-        self.debugging_log = options["debugging_log"]
-        self.trainingCrl = TrainingCrl(executor,
-                                       remote_control, trainer, trainingFunction)
+        with contextlib.suppress(Exception):
+            ctx = grpc.insecure_channel(config.grpc_address)
+            grpc_conn = grpc.blocking_channel(ctx)
+            logger.debug("Trying to connect to the server")
+            return cls(grpc_conn, logger), Remote(grpc_conn)
 
-        self.trainingCrl.logger = self._debug
-        self.gameServerAddress = None
-        self.helperPlayers = None
-        self.players = []
+        raise ConnectionError("Did not connect to the game server")
 
-    def start(self, lugo_client: client.LugoClient, executor: ThreadPoolExecutor):
-        hasStarted = False
+    def start(self, ctx: Any, trainer: "BotTrainer", training_function: "TrainingFunction") -> None:
+        self.grpc_conn.close()
+        training_ctrl = TrainingCrl(ctx, trainer, self.remote, self.assistant)
 
-        def play_callback(orderSet, snapshot):
-            nonlocal hasStarted
-            hasStarted = True
-            return self.trainingCrl.gameTurnHandler(orderSet, snapshot)
+        self.logger.debug("Starting training session")
+        session = self.assistant.StartSession(ctx, RLSessionConfig())
 
-        def trigger_listening() -> None:
-            nonlocal hasStarted
-            if hasStarted is False:
-                waiter = threading.Event()
-                executor.submit(self.remoteControl.resume_listening, waiter)
-                waiter.wait()
+        if session.Context().Err():
+            raise RuntimeError("Could not start a training session")
 
-        def on_join() -> None:
-            self._debug('The main bot is connected!! Starting to connect the zombies')
-            time.sleep(0.2)
-            if self.gameServerAddress:
-                self.players = self.helperPlayers(self.gameServerAddress, executor)
-            self._debug('helpers are done')
-            trigger_listening()
+        def keep_alive():
+            while True:
+                try:
+                    session.Recv()
+                except grpc.RpcError as err:
+                    self.logger.error(f"The RL assistant session ended with error: {err}")
+                    return
 
-        lugo_client.play(executor, play_callback, on_join)
-        return lugo_client
-
-    def stop(self):
-        self.trainingCrl.stop()
-        for player in self.players:
-            player.stop()
-
-    def with_zombie_players(self, game_server_address):
-        self._debug('Entering with_zombie_players\n')
-        self.gameServerAddress = game_server_address
-        self.helperPlayers = create_helper_players
-        return self
-
-    def withChasersPlayers(self, game_server_address):
-        self.gameServerAddress = game_server_address
-
-        def helper_players(game_server_address):
-            for i in range(1, 12):
-                newChaserHelperPlayer(Team.Side.HOME, i, game_server_address)
-                newChaserHelperPlayer(Team.Side.AWAY, i, game_server_address)
-
-        self.helperPlayers = helper_players(game_server_address)
-        return self
-
-    def _debug(self, message: str):
-        if self.debugging_log:
-            t = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())
-            print(f"[Debugger {t}] {message}")
-
-
-def create_helper_players(gameServerAddress: str, executor: ThreadPoolExecutor):
-    children = []
-    for i in range(0, 11):
-        time.sleep(0.01)
-        children.append(newZombieHelperPlayer(Team.Side.HOME, i + 1, gameServerAddress, executor))
-        time.sleep(0.01)
-        children.append(newZombieHelperPlayer(Team.Side.AWAY, i + 1, gameServerAddress, executor))
-    return children
+        self.logger.info("Session started")
+        training_function(training_ctrl)
--- src/lugo4py/rl/src/training_controller.py
+++ src/lugo4py/rl/src/training_controller.py
@@ -1,149 +1,52 @@
-import asyncio
-from .remote_control import RemoteControl
-from .interfaces import TrainingController, BotTrainer, TrainingFunction
-from ...protos.server_pb2 import GameSnapshot, OrderSet
-import threading
-from concurrent.futures import ThreadPoolExecutor
+import contextlib
+import grpc
 import time
+from typing import Any
+
+from src.lugo4py.protos.remote_pb2_grpc import Remote
+from src.lugo4py.protos.rl_assistant_pb2 import RLResetConfig
+from src.lugo4py.protos.rl_assistant_pb2_grpc import RLAssistant
+from src.lugo4py.rl.src.contracts import BotTrainer
 
 
+class TrainingCrl:
+    def __init__(self, ctx: Any, trainer: BotTrainer, remote: Remote, assistant: RLAssistant):
+        self.remote = remote
+        self.assistant = assistant
+        self.training_context = ctx
+        self.trainer = trainer
+        self.latest_snapshot = None
 
-
-class TrainingCrl(TrainingController):
-
-    def __init__(self, executor: ThreadPoolExecutor, remoteControl: RemoteControl, bot: BotTrainer, onReadyCallback: TrainingFunction):
-        self._gotNextState = lambda snapshot: print("got first snapshot")
-        self.logger = lambda msg: print(f'set debugger')
-        self.previousState = None
-        self.remoteControl = remoteControl  # type: RemoteControl
-        self.onReady = onReadyCallback
-        self.trainingHasStarted = False
-        self.lastSnapshot = None  # type: GameSnapshot
-        self.onListeningMode = False
-        self.OrderSet = None
-        self.cycleSeq = 0
-        self.bot = bot  # type: BotTrainer
-        self.debugging_log = False
-        self.stopRequested = threading.Event()
-        self.trainingExecutor = executor
-        self.resumeListeningPhase = lambda action: print(
-            'resumeListeningPhase not defined yet - should wait the initialise it on the first "update" call')
-
-    def set_environment(self, data):
-        self.logger('Reset state')
+    def set_environment(self, data: Any) -> None:
         try:
-            self.lastSnapshot = self.bot.set_environment(data)
-            return self.bot.get_state(self.lastSnapshot)
+            self.latest_snapshot = self.trainer.create_new_initial_state(data)
         except Exception as e:
-            print('bot trainer failed to create initial state: ', e)
-            raise e
+            raise RuntimeError(f"Failed to set environment: {e}")
 
-    def get_state(self):
+        with grpc.insecure_channel(self.assistant) as channel:
+            ctx = grpc.blocking_channel(channel)
+            time.sleep(10)  # Simulating timeout
+
+            print("**** Environment reset ******")
+            try:
+                self.assistant.ResetEnv(ctx, RLResetConfig())
+            except grpc.RpcError as e:
+                raise RuntimeError(f"Failed to reset RL assistant: {e}")
+
+    def get_state(self) -> Any:
+        return self.trainer.get_training_state(self.latest_snapshot)
+
+    def update(self, action: Any) -> tuple[float, bool]:
         try:
-            self.cycleSeq = self.cycleSeq + 1
-            self.logger('get state')
-            return self.bot.get_state(self.lastSnapshot)
+            players_orders = self.trainer.play(self.latest_snapshot, action)
         except Exception as e:
-            print('bot trainer failed to return inputs from a particular state', e)
-            raise e
-
-    def update(self, action: any):
-        self.logger('received action from training bot')
-        if not self.onListeningMode:
-            raise ValueError('faulty synchrony - got a new action when was still processing the last one')
+            raise RuntimeError(f"Trainer bot failed to play: {e}")
 
         try:
-            previousState = self.lastSnapshot
-            self.OrderSet.turn = self.lastSnapshot.turn
-            updatedOrderSet = self.bot.play(self.OrderSet, self.lastSnapshot, action)
+            turn_outcome = self.assistant.SendPlayersOrders(self.training_context, players_orders)
+        except grpc.RpcError as e:
+            raise RuntimeError(f"RL assistant failed to send the orders: {e}")
 
-            self.logger('got order set, passing down')
-
-            self.resumeListeningPhase(updatedOrderSet)
-            #time.sleep(2.4)  # before calling next turn, let's wait just a bit to ensure the server got our order
-            self.lastSnapshot = self.wait_until_next_listening_state()
-
-            self.logger('got new snapshot after order has been sent')
-
-            if self.stopRequested.is_set():
-                return None
-
-            # TODO: if I want to skip the net N turns? I should be able too
-            self.logger(f"update finished (turn {self.lastSnapshot.turn} waiting for next action)")
-        except Exception as e:
-            print('failed send new action to the server: ', e)
-            raise e
-
-        try:
-            return self.bot.evaluate(previousState, self.lastSnapshot)
-        except Exception as e:
-            print('bot trainer failed to evaluate game state', e)
-            raise e
-
-    def gameTurnHandler(self, order_set, snapshot):
-        if self.stopRequested.is_set():
-            self.logger('skipping turn handler because the stop request')
-            return None
-        self.logger('new turn')
-        if self.onListeningMode:
-            raise RuntimeError(
-                "faulty synchrony - got new turn while waiting for order (check the lugo 'timer-mode')")
-
-        self._gotNextState(snapshot)
-        self.OrderSet = order_set
-
-        waiter = threading.Event()
-        new_order_set = None
-
-        def resume(updated_order_set):
-            nonlocal new_order_set
-            new_order_set = updated_order_set
-            waiter.set()
-            self.logger(f'Sending new action')
-
-        self.resumeListeningPhase = resume
-        self.onListeningMode = True
-        if self.trainingHasStarted is False:
-            self.trainingExecutor.submit(self.onReady, self, self.stopRequested)
-            self.trainingHasStarted = True
-            self.logger(f'the training has started')
-
-        self.logger(f'Waiting get new update!')
-        waiter.wait(timeout=5)
-        self.logger(f'order sent to the game server')
-        return new_order_set
-
-    def wait_until_next_listening_state(self) -> GameSnapshot:
-        try:
-            self.onListeningMode = False
-            waiter = threading.Event()
-
-            new_snapshot = None
-            def resume(newGameSnapshot):
-                nonlocal new_snapshot
-                new_snapshot = newGameSnapshot
-                waiter.set()
-
-            self._gotNextState = resume
-
-
-            waiterResumeListening = threading.Event()
-            self.trainingExecutor.submit(self.remoteControl.resume_listening, waiterResumeListening)
-            waiterResumeListening.wait()
-
-            waiter.wait(timeout=3)
-            if new_snapshot is None:
-                raise RuntimeError(
-                    "timed out waiting for the next listening state - check the training controller")
-
-            self.logger(f'resume_listening: {new_snapshot.turn}')
-
-            return new_snapshot
-        except Exception as e:
-            self.logger(f'failed to send the orders to the server {e}')
-            raise
-
-    def stop(self):
-        self.stopRequested.set()
-
-
+        previous_snapshot = self.latest_snapshot
+        self.latest_snapshot = turn_outcome.game_snapshot
+        return self.trainer.evaluate(previous_snapshot, turn_outcome.game_snapshot, turn_outcome)
