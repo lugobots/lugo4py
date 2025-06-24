ae95d9f54f3d64f02cc84575d1d449d7e8a0e69c
Rubens Silva <rubens21@gmail.com>
2025-06-23 21:59:39 -0600

    documenting

--- example/rl/my_bot.py
+++ example/rl/my_bot.py
@@ -27,7 +27,13 @@ class MyBotTrainer(BotTrainer):
         self.Mapper = None
 
     def create_new_initial_state(self, data: Any) -> GameSnapshot:
+        '''
+        It is called everytime the environment must be reset. Here you can change the game element positions
 
+        :param data: This parâmeter can be anything YOU want to pass during in the training function. It is whatever is
+         passed to set_environment method.
+        :return: a game snapshot got from the game server
+        '''
         player_prop = PlayerProperties()
         player_prop.number = 5
         player_prop.side = Team.HOME
@@ -38,7 +44,14 @@ class MyBotTrainer(BotTrainer):
         return response.game_snapshot
 
     def get_training_state(self, snapshot: GameSnapshot) -> Any:
-        # TODO explain this
+        '''
+        It is called when "training_ctrl.get_state()" is called in the training function. You may return the data in any
+         format
+        It will depend on your own implementation.
+        :param snapshot: Gets the current state of the game snapshot.
+        :return: It will depend on your own implementation. It is whatever you consider to be the state of your training
+         sensors
+        '''
         return [True, True, False]
 
     def play(self, game_snapshot: GameSnapshot, action: Any) -> PlayersOrders:
@@ -58,6 +71,12 @@ class MyBotTrainer(BotTrainer):
         return response
 
     def evaluate(self, previous_game_snapshot: GameSnapshot, new_game_snapshot: GameSnapshot, turn_outcome: TurnOutcome    ) -> Tuple[float, bool]:
+        '''
+        :param previous_game_snapshot:
+        :param new_game_snapshot:
+        :param turn_outcome:
+        :return:
+        '''
         return random.random(), new_game_snapshot.turn >= 600
 
 
